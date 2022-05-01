7a5c4cb79c09db1dae54b63bb25edf390fa5d481
Hamik Hambardzumyan <hamikhambardzumyan@Hamiks-MacBook-Pro.local>
2022-05-01 15:42:59 +0400

    Remove support of CJS support

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
 import url from 'rollup-plugin-url';
@@ -9,12 +8,6 @@ import pkg from './package.json';
 export default {
     input: 'src/index.ts',
     output: [
-        {
-            file: pkg.main,
-            format: 'cjs',
-            exports: 'named',
-            sourcemap: true
-        },
         {
             file: pkg.module,
             format: 'es',
@@ -29,7 +22,6 @@ export default {
         typescript({
             rollupCommonJSResolveHack: true,
             clean: true
-        }),
-        commonjs()
+        })
     ]
 };
